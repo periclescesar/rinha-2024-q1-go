b9061293226c7b7bac6bbe7774f041dd3153e35e
periclescesar <periclescesar@gmail.com>
2024-02-20 01:51:33 -0300

    test(feature): create triggers of the feature steps

---
+++ api_test.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/cucumber/godog"
+	messages "github.com/cucumber/messages/go/v21"
+	httpHandler "github.com/periclescesar/rinha-2024-q1-go/internal/clientes/delivery/http"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+)
+
+type apiFeature struct {
+	resp *httptest.ResponseRecorder
+}
+
+func (a *apiFeature) resetResponse(*messages.Pickle) {
+	a.resp = httptest.NewRecorder()
+}
+
+func (a *apiFeature) iMakeADebitOfToTheCustomersAccountWithIdAndDescription(amount, id int, description string) error {
+	data := map[string]interface{}{
+		"valor":     amount,
+		"tipo":      'd',
+		"descricao": description,
+	}
+	jsonBody, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Erro ao converter para JSON:", err)
+		return err
+	}
+
+	body := bytes.NewReader(jsonBody)
+	return a.makeRequest(http.MethodPost, fmt.Sprintf("/clientes/%d/transacoes", id), body)
+}
+
+func (a *apiFeature) iWillReceiveAError(errMessage string) error {
+	expected := map[string]interface{}{
+		"mensagem": errMessage,
+	}
+
+	var actual interface{}
+	// re-encode actual response too
+	if err := json.Unmarshal(a.resp.Body.Bytes(), &actual); err != nil {
+		return err
+	}
+
+	return assertExpectedAndActual(assert.Equal, expected, actual)
+}
+
+type Balance struct {
+	Total       int    `json:"total"`
+	DataExtrato string `json:"data_extrato"`
+	Limite      int    `json:"limite"`
+}
+
+type Transaction struct {
+	Valor      int    `json:"valor"`
+	Tipo       string `json:"tipo"`
+	Descricao  string `json:"descricao"`
+	RealizedAt string `json:"realizada_em"`
+}
+
+type AccountStattment struct {
+	Saldo             interface{}   `json:"saldo"`
+	UltimasTransacoes []Transaction `json:"ultimas_transacoes"`
+}
+
+func (a *apiFeature) iWillSeeMyLimitOfAndBalanceOf(limit, balance int) error {
+	var expected = &AccountStattment{
+		Saldo: &Balance{
+			Limite: limit,
+			Total:  balance,
+		},
+	}
+	var actual AccountStattment
+	// re-encode actual response too
+	if err := json.Unmarshal(a.resp.Body.Bytes(), &actual); err != nil {
+		return err
+	}
+
+	return assertExpectedAndActual(assert.Equal, expected, actual)
+}
+
+func (a *apiFeature) theLastsTransactions(nTransactions int) error {
+	var actual AccountStattment
+	// re-encode actual response too
+	if err := json.Unmarshal(a.resp.Body.Bytes(), &actual); err != nil {
+		return err
+	}
+
+	return assertExpectedAndActual(assert.Equal, nTransactions, len(actual.UltimasTransacoes))
+}
+
+func (a *apiFeature) getAccountStattmentOfCustomer(id int) error {
+	return a.makeRequest("GET", fmt.Sprintf("/clientes/%d/extrato", id), nil)
+}
+
+func (a *apiFeature) makeRequest(method string, path string, body io.Reader) error {
+	req, err := http.NewRequest(method, path, body)
+	if err != nil {
+		return err
+	}
+
+	r := httpHandler.SetupRouter()
+	r.ServeHTTP(a.resp, req)
+
+	defer func() {
+		switch t := recover().(type) {
+		case string:
+			err = fmt.Errorf(t)
+		case error:
+			err = t
+		}
+	}()
+
+	return err
+}
+
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	api := &apiFeature{}
+	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
+		api.resetResponse(sc)
+		return ctx, nil
+	})
+
+	ctx.Step(`^I get a account stattement of the customer\'s id (\d+)$`, api.getAccountStattmentOfCustomer)
+	ctx.Step(`^I make a debit of (\d+) to the customer\'s account with id (\d+) and description "([^"]*)"$`, api.iMakeADebitOfToTheCustomersAccountWithIdAndDescription)
+	ctx.Step(`^I will receive a error "([^"]*)"$`, api.iWillReceiveAError)
+	ctx.Step(`^I will see my limit of (\d+) and balance of -(\d+)$`, api.iWillSeeMyLimitOfAndBalanceOf)
+	ctx.Step(`^the lasts (\d+) transactions$`, api.theLastsTransactions)
+}
---
+++ main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
+	"github.com/periclescesar/rinha-2024-q1-go/configs"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+var opts = godog.Options{
+	Output: colors.Colored(os.Stdout),
+	Format: "progress", // can define default values
+}
+
+func init() {
+	configs.InitConfigs(".test.env")
+
+	godog.BindCommandLineFlags("godog.", &opts) // godog v0.11.0 (latest)
+}
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
+}
+
+// assertExpectedAndActual is a helper function to allow the step function to call
+// assertion functions where you want to compare an expected and an actual value.
+func assertExpectedAndActual(a expectedAndActualAssertion, expected, actual interface{}, msgAndArgs ...interface{}) error {
+	var t asserter
+	a(&t, expected, actual, msgAndArgs...)
+	return t.err
+}
+
+type expectedAndActualAssertion func(t assert.TestingT, expected, actual interface{}, msgAndArgs ...interface{}) bool
+
+// asserter is used to be able to retrieve the error reported by the called assertion
+type asserter struct {
+	err error
+}
+
+// Errorf is used by the called assertion to report an error
+func (a *asserter) Errorf(format string, args ...interface{}) {
+	a.err = fmt.Errorf(format, args...)
+}
