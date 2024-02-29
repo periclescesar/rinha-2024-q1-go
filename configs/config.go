269ddeaee6d8319d4c13f1d87515afda53150292
periclescesar <periclescesar@gmail.com>
2024-02-29 00:10:16 -0300

    feat(database): create database connection

--- configs/config.go
+++ configs/config.go
@@ -9,8 +9,17 @@ type ApiConf struct {
 	Port int
 }
 
+type DbConf struct {
+	Host   string
+	Port   int
+	User   string
+	Pass   string
+	Dbname string
+}
+
 type config struct {
 	ApiConf *ApiConf
+	DbConf  *DbConf
 }
 
 var Configs *config
@@ -19,9 +28,20 @@ func InitConfigs(configFile string) {
 	InitEnv(configFile)
 	Configs = new(config)
 	Configs.ApiConf = buildApiConfig()
+	Configs.DbConf = buildDbConfig()
 }
 
 func buildApiConfig() *ApiConf {
 	var port, _ = strconv.Atoi(viper.Get("PORT").(string))
 	return &ApiConf{Port: port}
+}
+
+func buildDbConfig() *DbConf {
+	var port, _ = strconv.Atoi(viper.Get("DB_PORT").(string))
+	var host, _ = viper.Get("DB_HOST").(string)
+	var user, _ = viper.Get("DB_USER").(string)
+	var pass, _ = viper.Get("DB_PASS").(string)
+	var dbname, _ = viper.Get("DB_DBNAME").(string)
+
+	return &DbConf{Host: host, Port: port, User: user, Pass: pass, Dbname: dbname}
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/cucumber/godog v0.14.0
 	github.com/cucumber/messages/go/v21 v21.0.1
 	github.com/gin-gonic/gin v1.9.1
-	github.com/juliangruber/go-intersect v1.1.0
+	github.com/lib/pq v1.10.9
+	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.18.2
 	github.com/stretchr/testify v1.8.4
 )
@@ -44,7 +45,6 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
